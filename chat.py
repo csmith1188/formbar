79a4caf7111aa6c9ed38ed079a301f29c30c89cd
ChrisMooney04 <Chris2022058@live.ytech.edu>
2021-03-25 14:53:18 -0400

    Fixed Chat Program

--- chat.py
+++ chat.py
@@ -1,15 +1,26 @@
-import asyncio
-import websockets
+from websocket_server import WebsocketServer
 
-async def hello(websocket, path):
-    name = await websocket.recv()
-    print(f"< {name}")
+# Called for every client connecting (after handshake)
+def new_client(client, server):
+	print("New client connected and was given id %d" % client['id'])
+	
 
-    greeting = f"Hello {name}!"
 
-    await websocket.send(greeting)
-    print(f"> {greeting}")
-    
-start_server = websockets.serve(hello, "localhost", 5001)
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+# Called for every client disconnecting
+def client_left(client, server):
+	print("Client(%d) disconnected" % client['id'])
+
+
+# Called when a client sends a message
+def message_received(client, server, message):
+	if len(message) > 200:
+		message = message[:200]+'..'
+	print("Client(%d) said: %s" % (client['id'], message))
+
+
+PORT=9001
+server = WebsocketServer(PORT)
+server.set_fn_new_client(new_client)
+server.set_fn_client_left(client_left)
+server.set_fn_message_received(message_received)
+server.run_forever()

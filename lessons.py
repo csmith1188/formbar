ef4db368639a1c84de6aaebb84bbc3d990d2743c
csmith1188 <csmith@ytech.edu>
2021-05-03 12:10:43 -0400

    first commit

--- lessons.py
+++ lessons.py
@@ -1,52 +1,74 @@
 import pandas#Imports pandas.
 import os#Imports operating system.
 
-def readFolder():
-    lessonList = []
+class Lesson():
+    def __init__(self):
+        pass
+    lessonModel = {
+        'agenda': [{'time': 0, 'title': '', 'desc': '', 'step': 0}],
+        'objectives': [{'objective': '', 'desc': ''}],
+        'links': [{'url': '', 'desc': ''}],
+        'quizzes': {'Quiz_1': {'name': '', 'questions':[], 'keys': [], 'answers': []}},
+        'progLists': {'Progress_1': {'name': '', 'task':[], 'desc': []}}
+    }
+
+lessons = {}
+
+def updateFiles():
+    global lessons
+    #Empty lessons file list
+    lessons = {}
     #Scan folder for all filenames
     availableFiles = os.listdir("./lessondata")
     #Loop through each file
     for file in sorted(availableFiles):
-        #Check last four letters are the correct file extension
-        if file[-5:].lower() == '.xlsx':
-            lessonList.append(readBook(pandas.ExcelFile('lessondata/' + file)))
-    return lessonList
+        #Check last five letters are the correct file extension
+        if file[-5:] == '.xlsx':
+            #Add them to the lessonsFiles list if so
+            lessons[file[:-5]] = "/home/pi/formbar/lessondata/" + file
 
-def readBook(book):
+def readBook(incBook):
     # newLesson = newBook.parse('Quiz_1').to_dict()
-    lessonData = {
-        'bellringer': {},
-        'exitticket': {},
-        'progressList': [],
-        'quizList': []
-    }
+    newBook = 'lessondata/' + incBook + '.xlsx'
+    book = pandas.ExcelFile(newBook)
+    lessonData = Lesson()
     for sheet in book.sheet_names:
-        if sheet[0:5] == 'Quiz_':
-            lesson = book.parse(sheet).to_dict()
-            quiz = {'questions':[], 'keys': [], 'answers': []}
+        if sheet == 'Agenda':
+            data = book.parse(sheet).to_dict()
+            print(data)
+            pass
+        elif sheet == 'Steps':
+            data = book.parse(sheet).to_dict()
+            print(data)
+            pass
+        elif sheet == 'Objectives':
+            data = book.parse(sheet).to_dict()
+            print(data)
+            pass
+        elif sheet == 'Resources':
+            data = book.parse(sheet).to_dict()
+            print(data)
+            pass
+        elif sheet[0:5] == 'Quiz_':
+            data = book.parse(sheet).to_dict()
+            quiz = {'name': sheet[5:], 'questions':[], 'keys': [], 'answers': []}
             for row in range(0, len(lesson['Question'])):
                 answers = []
-                for i, col in enumerate(lesson):
+                for i, col in enumerate(data):
                     if i == 0:
-                        quiz['questions'].append(lesson[col][row])
+                        quiz['questions'].append(data[col][row])
                     if i == 1:
-                        quiz['keys'].append(lesson[col][row])
+                        quiz['keys'].append(data[col][row])
                     elif i > 1:
-                        answers.append(lesson[col][row])
+                        answers.append(data[col][row])
                 quiz['answers'].append(answers)
             lessonData['quizList'].append(quiz)
+        elif sheet[0:9] == 'Progress_':
+            data = book.parse(sheet).to_dict()
+            progress = {'name': sheet[9:], 'task':[], 'desc': []}
+            for task in data['Task']:
+                progress['task'].append(data['Task'][task])
+            for desc in data['Description']:
+                progress['desc'].append(data['Description'][desc])
+            lessonData['progressList'].append(progress)
     return lessonData
-
-# def consoleQuiz():
-#     for quiz in lessonData['quizList']:
-#         for i, question in enumerate(quiz['questions']):
-#             print(question)
-#             for j, answer in enumerate(quiz['answers'][i]):
-#                 print(j + 1, ": ", answer)
-#             userAnswer = input('What is? ')
-#             if int(userAnswer) == quiz['keys'][i]:
-#                 print('Correct!')
-#             else:
-#                 print('haha dumb!')
-
-print(readFolder())

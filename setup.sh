90ec03c393032e1ff5263be09ba1687b4c548a96
SMITH <CSMITH@ytech.edu>
2021-08-16 11:53:34 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-sudo apt install python3-pip
+#install packages
+sudo apt update
+sudo apt upgrade
+sudo apt-get -y install python3-pip
+sudo apt-get -y install libatlas-base-dev
+sudo apt-get -y install libsdl-ttf2.0-0
+sudo apt-get -y install python3-sdl2
+#install modules
 sudo pip3 install rpi_ws281x adafruit-circuitpython-neopixel
 sudo python3 -m pip install --force-reinstall adafruit-blinka
-#install modules
 sudo pip3 install flask
 sudo pip3 install websocket_server
+sudo pip3 install pygame
+sudo pip3 install openpyxl
+sudo pip3 install netifaces
 sudo pip3 --default-timeout=1000 install pandas
-sudo apt-get install libatlas-base-dev
-sudo pip3 openpyxl
+

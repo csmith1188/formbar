a34a550efab4fa2b9976a7a7c994c41d7667c9db
SMITH <CSMITH@ytech.edu>
2021-08-16 11:06:36 -0400

    Setup Script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-#install board
-#install neopxiels
+sudo apt install python3-pip
+sudo pip3 install rpi_ws281x adafruit-circuitpython-neopixel
+sudo python3 -m pip install --force-reinstall adafruit-blinka
 #install modules
+sudo pip3 install flask
+sudo pip3 install websocket_server
 sudo pip3 --default-timeout=1000 install pandas
 sudo apt-get install libatlas-base-dev
-#install openpyxl
+sudo pip3 openpyxl

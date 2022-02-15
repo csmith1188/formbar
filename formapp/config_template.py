0cf157eabd9d3a8dcacb68d5c9a3b41cf33f5363
Mr. Smith <csmith@ytech.edu>
2022-02-15 14:00:06 -0500

    First Commit

--- formapp/config_template.py
+++
@@ -1,8 +0,0 @@
-#Set to True for RPi, False for PC
-ONRPi = True
-#Enable/Disable dbug() blurbs
-DEBUG = False
-#Set the maximum number of pixels on the bar
-BARPIX = 300
-#Set the maximum number of pixels, including pixelpanels
-MAXPIX = 812

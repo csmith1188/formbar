bc8b5cecf7d07585c09f082f337cc94ed24c4d99
Oley Birkeland <john2022026@live.ytech.edu>
2022-04-07 12:24:57 -0400

    Fix indentation

--- formapp/app.py
+++ formapp/app.py
@@ -1773,12 +1773,12 @@ def endpoint_survey():
 
 @app.route('/towerdefense')
 def endpoint_towerdefense():
-if not request.remote_addr in sD.studentDict:
-    return redirect('/login?forward=' + request.path)
-if sD.studentDict[request.remote_addr]['perms'] > sD.settings['perms']['games']:
-    return render_template("message.html", message = "You do not have high enough permissions to do this right now. " )
-else:
-    return render_template('towerdefense.html')
+    if not request.remote_addr in sD.studentDict:
+        return redirect('/login?forward=' + request.path)
+    if sD.studentDict[request.remote_addr]['perms'] > sD.settings['perms']['games']:
+        return render_template("message.html", message = "You do not have high enough permissions to do this right now. " )
+    else:
+        return render_template('towerdefense.html')
 
 #Tic Tac Toe
 @app.route('/ttt')

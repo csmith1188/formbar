028fee76e473a44b410d0f8e68cee3eab2eab7d1
Mr. Smith <csmith@ytech.edu>
2022-04-07 13:11:44 -0400

    Preparing for next RC

--- formapp/modules/bot_ir.py
+++ formapp/modules/bot_ir.py
@@ -7,10 +7,10 @@ time.sleep(1)
 #This will be changed once there is a better login method in the main app.py
 
 #Send a login POST request
-print(requests.post(url="http://localhost:5000/login", data={"username":"Formbar Aux", "password":"yes", "bot":"true", "forward":"/"}).text)
+print(requests.post(url="http://localhost:420/login", data={"username":"Formbar Aux", "password":"yes", "bot":"true", "forward":"/"}).text)
 #This should instead constantly poll to see if it is already logged in or not
 
-print(requests.get('http://localhost:5000/sfx?file=sfx_up03').text)
+print(requests.get('http://localhost:420/sfx?file=sfx_up03').text)
 
 #Handle the buttons forever
 print("Scanning for button presses...")
@@ -21,13 +21,13 @@ while True:
             # print(ir.ButtonsNames[button]) #Prints corresponding english name for button
             if ir.ButtonsNames[button] == 'power':
                 print("[Power] Flush all students from class")
-                requests.get('http://localhost:5000/flush')
+                requests.get('http://localhost:420/flush')
             elif ir.ButtonsNames[button] == 'func':
                 pass
                 #changeMode()
             elif ir.ButtonsNames[button] == 'repeat':
                 pass
-                requests.post(url="http://localhost:5000/settings", data={})
+                requests.post(url="http://localhost:420/settings", data={})
             elif ir.ButtonsNames[button] == 'rewind':
                 pass
                 #rewindBGM()
@@ -35,12 +35,12 @@ while True:
                 pass
                 #playpauseBGM()
             elif ir.ButtonsNames[button] == 'eq':
-                requests.get('http://localhost:5000/sfx?file=sfx_up03')
+                requests.get('http://localhost:420/sfx?file=sfx_up03')
             elif ir.ButtonsNames[button] == 'vol_up':
-                requests.get('http://localhost:5000/bgm?voladj=up')
+                requests.get('http://localhost:420/bgm?voladj=up')
             elif ir.ButtonsNames[button] == 'vol_down':
-                requests.get('http://localhost:5000/bgm?voladj=down')
+                requests.get('http://localhost:420/bgm?voladj=down')
             elif ir.ButtonsNames[button] == 'up':
-                requests.get('http://localhost:5000/lesson?action=next')
+                requests.get('http://localhost:420/lesson?action=next')
             elif ir.ButtonsNames[button] == 'down':
-                requests.get('http://localhost:5000/lesson?action=prev')
+                requests.get('http://localhost:420/lesson?action=prev')

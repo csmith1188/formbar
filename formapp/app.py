f940277e92836ce3e3c6e82d15eaaba89edcfe71
Oley Birkeland <john2022026@live.ytech.edu>
2022-04-25 14:34:11 -0400

    /help update & "return" param

--- formapp/app.py
+++ formapp/app.py
@@ -1400,29 +1400,28 @@ def endpoint_hangman():
             highScore = 0
         return render_template("hangman.html", wordObj=wordObj, highScore = highScore)
 
-@app.route('/help')
+@app.route('/help', methods = ['POST', 'GET'])
 def endpoint_help():
     if not request.remote_addr in sD.studentDict:
         return redirect('/login?forward=' + request.path)
     if sD.studentDict[request.remote_addr]['perms'] == sD.settings['perms']['teacher']:
         return redirect(sD.mainPage + "?alert=Teachers can't send help tickets.")
     else:
-        if request.args.get('action') == "send":
-            name = sD.studentDict[request.remote_addr]['name']
-            name = name.strip()
-            if name in helpList:
-                return redirect("/help?alert=You already have a help ticket or break request in. If your problem is time-sensitive, or your last ticket was not cleared, please get the teacher's attention manually." )
-            else:
-                helpList[name] = request.args.get('message') or '<i>Sent a help ticket</i>'
-                sD.studentDict[request.remote_addr]['help'] = True
-                playSFX("sfx_up04")
-                return redirect("/help?alert=Your ticket was sent. Keep working on the problem the best you can while you wait." )
+        name = sD.studentDict[request.remote_addr]['name']
+        name = name.strip()
+        if name in helpList:
+            return redirect("/help?alert=You already have a help ticket or break request in. If your problem is time-sensitive, or your last ticket was not cleared, please get the teacher's attention manually." )
+        elif request.method == 'POST':
+            helpList[name] = request.args.get('message') or '<i>Sent a help ticket</i>'
+            sD.studentDict[request.remote_addr]['help'] = True
+            playSFX("sfx_up04")
+            return redirect("/help?alert=Your ticket was sent. Keep working on the problem the best you can while you wait." )
         else:
             return render_template("help.html")
 
+
 @app.route('/home')
 def endpoint_home():
-    print("home")
     if not request.remote_addr in sD.studentDict:
         return redirect('/login?forward=' + request.path)
     else:
@@ -2068,7 +2067,7 @@ def endpoint_settings():
         resString = ''
         #Loop through every arg that was sent as a query parameter
         for arg in request.args:
-            if arg != "advanced":
+            if arg != 'advanced':
                 #See if you save the
                 argVal = str2bool(request.args.get(arg))
                 #if the argVal resolved to a boolean value

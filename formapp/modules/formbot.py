ee554e3dc069b1d0a42ba1e91bb63e1dee71778a
Trey <90394867+TFloto@users.noreply.github.com>
2022-10-13 10:50:30 -0400

    :3

--- formapp/modules/formbot.py
+++ formapp/modules/formbot.py
@@ -1,6 +1,39 @@
 import requests
 import time
 
+#Importing external modules
+from flask import Flask, redirect, url_for, request, render_template, session, copy_current_request_context
+from flask_socketio import SocketIO, emit, disconnect
+from threading import Lock
+from werkzeug.utils import secure_filename
+from websocket_server import WebsocketServer
+from cryptography.fernet import Fernet
+import pandas, json, csv
+import random, sys, os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+import pygame
+import time, math
+import threading
+import logging
+import traceback
+import sqlite3
+import board, neopixel
+
+#Importing customs modules
+from modules import ir
+
+# Change the built-in logging for flask
+flasklog = logging.getLogger('werkzeug')
+flasklog.setLevel(logging.ERROR)
+
+#Start up pygame for sfx and bgm
+pygame.init()
+
+#Start the neopixel tracker
+
+#Create a list of neopixels if we are on an RPi
+pixels = neopixel.NeoPixel(board.D21, MAXPIX, brightness=1.0, auto_write=False)
+
 class FormBot():
     def __init__(self, username, password, timeout=0, host='127.0.0.1', port=420)       :
         self.host = '192.168.10.69'

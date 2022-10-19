0b97a95a5b01485bd1a4a38dc47008616618566a
Mr. Smith <csmith@ytech.edu>
2022-10-19 09:17:24 -0400

    Revert "Revert "Merge branch 'RC' of https://github.com/csmith1188/formbar into RC""
    This reverts commit 57f3e003f52dbb4be1912f16449977b11c82568d.

--- formapp/modules/formbot.py
+++ formapp/modules/formbot.py
@@ -1,29 +1,24 @@
 import requests
 import time
 
-
 class FormBot():
-    def __init__(self, username, password, timeout=0, host='127.0.0.1', port=5000):
-        self.host = host
-        self.port = 5000
+    def __init__(self, username, password, timeout=0, host='127.0.0.1', port=420)       :
+        self.host = '192.168.10.69'
+        self.port = 420
         self.username = username
         self.password = password
         self.loggedin = False
         time.sleep(timeout)
         self.login()
+        print('logged in as', username)
 
+
+    #change "userType": "login", to "usertype": "new" on first time use
     def login(self):
-        loginAttempt = requests.post(url="http://"+self.host+":"+str(self.port)+"/login", data={"username": self.username, "password": self.password, "bot": "True", "forward":"/"})
+        loginAttempt = requests.post(url="http://"+self.host+":"+str(self.port)+"/login", data={"username": self.username, "password": self.password, "userType": "new", "bot": "True", "forward":"/"})
 
-    def tutd(self, thumb):
-        url = "http://"+self.host+":"+str(self.port)+"/tutd"
-        if thumb.lower() == 'up':
-            requests.get(url+'?thumb=up')
-        if thumb.lower() == 'down':
-            requests.get(url+'?thumb=down')
-        if thumb.lower() == 'wiggle':
-            requests.get(url+'?thumb=wiggle')
-        if thumb.lower() == 'oops':
-            requests.get(url+'?thumb=oops')
-        else:
-            pass
+
+
+#forbius login
+
+Forbius = FormBot('Forbius', 'Password#1')
